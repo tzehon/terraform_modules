c9cba6bee98a1fb4aeaddded0cd32721ba07d2a6
tze hon <tzehon.tan@gmail.com>
2023-12-04 17:17:53 -0500

    Add secret manager module

---
+++ services/secrets/variables.tf
@@ -0,0 +1,19 @@
+variable "project_id" {
+  description = "The Google Cloud project ID"
+  type        = string
+}
+
+variable "region" {
+  description = "The preferred GCP region"
+  type        = string
+}
+
+variable "secret_id" {
+  description = "The secret ID"
+  type        = string
+}
+
+variable "secret_value" {
+  description = "The secret value"
+  type        = string
+}
